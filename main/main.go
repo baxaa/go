539f5a4c6ec7a0402abbbd05b59cea531664658b
baxaa <onalbekovbakbergen7@gmail.com>
2023-02-17 18:24:29 +0600

    asdasd

--- main/main.go
+++ main/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"github.com/baxaa/e-commerce/CRUD"
 )
 
 func main() {
-	http.HandleFunc("/create", createUser)
-	http.ListenAndServe(":8181", nil)
-	// CRUD.Indexhandler()
+	CRUD.Routes()
 }
