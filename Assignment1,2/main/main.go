e688691128177dbadb9a17a941e73d18cfd581c7
baxaa <onalbekovbakbergen7@gmail.com>
2023-03-13 17:13:39 +0600

    upd

--- Assignment1,2/main/main.go
+++ Assignment1,2/main/main.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"github.com/baxaa/e-commerce/Assignment1,2/CRUD"
-	// "database/sql"
-	// "fmt"
-	_ "github.com/lib/pq"
 )
 
-const (
-	user     = "postgres"
-	password = "uhupup667"
-	dbname   = "go"
-)
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+//const (
+//	user     = "postgres"
+//	password = "uhupup667"
+//	dbname   = "go"
+//)
+//
+//func check(err error) {
+//	if err != nil {
+//		panic(err)
+//	}
+//}
 
 func main() {
 	CRUD.Routes()
